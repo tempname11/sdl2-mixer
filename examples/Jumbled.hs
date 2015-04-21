31509dce57e6304b76ad6f4fc604e54dd6310011
sbidin <sinisa@bidin.cc>
2015-04-21 02:16:55 +0200

    Add playing files to example.

--- examples/Jumbled.hs
+++ examples/Jumbled.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.Default.Class (def)
+import System.Environment (getArgs)
+import System.Exit        (exitFailure)
 
 import qualified SDL
 import qualified SDL.Mixer
@@ -8,15 +10,25 @@ import qualified SDL.Mixer
 main :: IO ()
 main = do
   SDL.initialize [SDL.InitAudio]
-  SDL.Mixer.initialize [SDL.Mixer.InitMP3]
   SDL.Mixer.openAudio def 256
 
   putStr "Available chunk decoders: "
   print =<< SDL.Mixer.chunkDecoders
 
-  putStr "Available music decoders: "
-  print =<< SDL.Mixer.musicDecoders
+  args <- getArgs
+  case args of
+    [] -> putStrLn "usage: cabal run AUDIO_FILE_PATH..." >> exitFailure
+    xs -> runExample xs
 
   SDL.Mixer.closeAudio
-  SDL.Mixer.quit
   SDL.quit
+
+-- | Play each of the sounds at the same time!
+runExample :: [FilePath] -> IO ()
+runExample paths = do
+  SDL.Mixer.setChannels $ length paths
+  chunks <- mapM SDL.Mixer.load paths
+  mapM_ (SDL.Mixer.play SDL.Mixer.AnyChannel SDL.Mixer.Once) chunks
+  SDL.delay 2000
+  SDL.Mixer.setChannels 0
+  mapM_ SDL.Mixer.free chunks
